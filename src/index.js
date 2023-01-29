85de6d57c21be2db134511738082391b6fe03946
Murilo Thom <murilothom.2002@hotmail.com>
2023-01-29 10:44:41 -0300

    feat: create user, middleware to verify if user exist and get todos

--- src/index.js
+++ src/index.js
@@ -1,25 +1,56 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+
+  if (!users.some(user => user.username === username)) {
+    return response.status(400).json({ error: 'User not found.' })
+  }
+
+  request.username = username
+
+  return next()
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body
+
+  if (!name || !username) {
+    return response.status(400).json({ error: 'Missing data.' })
+  }
+
+  if (users.some(user => user.name === name)) {
+    return response.status(400).json({ error: 'User already exists.' })
+  }
+
+  const user = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: []
+  }
+
+  users.push(user)
+
+  return response.status(201).send()
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { username } = request
+
+  const user = users.find(user => user.username === username)
+
+  return response.json(user.todos)
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
