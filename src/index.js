918a760e03af03067623ddc935f1dfcf54145abd
Murilo Thom <murilothom.2002@hotmail.com>
2023-01-29 10:51:15 -0300

    feat: create todos

--- src/index.js
+++ src/index.js
@@ -54,7 +54,22 @@ app.get('/todos', checksExistsUserAccount, (request, response) => {
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body
+  const { username } = request
+
+  const todo = { 
+    id: uuidv4(),
+    title,
+    done: false, 
+    deadline: new Date(deadline), 
+    created_at: new Date()
+  }
+
+  const user = users.find(user => user.username === username)
+
+  user.todos.push(todo)
+
+  return response.status(201).send()
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
